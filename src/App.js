55338dc5eeed6c220d0ad0d1d465991a99ca25c2
badahae88 <badahae88@gmail.com>
2023-06-28 01:04:27 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  let [count, setCount] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Counter</h1>
+      <div className='CountBox'>
+        <h2>현재 카운트:</h2>
+        <h1>{count}</h1>
+
+        <div className='EvenOdd'>
+          <p>현재 카운트는</p>
+          {
+            (count%2) ? <p>홀수</p> : <p>짝수</p>
+          }
+        <p>입니다</p>
+        </div>
+      </div>
+
+      <button onClick={()=>{
+        setCount(count-1);
+      }}>-1</button>
+      <button onClick={()=>{
+        setCount(count-10);
+      }}>-10</button>
+      <button onClick={()=>{
+        setCount(count-100);
+      }}>-100</button>
+      <button onClick={()=>{
+        setCount(count+1);
+      }}>+1</button>
+      <button onClick={()=>{
+        setCount(count+10);
+      }}>+10</button>
+      <button onClick={()=>{
+        setCount(count+100);
+      }}>+100</button>
     </div>
   );
 }
